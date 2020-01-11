2aecd49423da64adfb6d929da28d8ae2858708fc
th3jiv3r <th3jiv3r@goodlandsecurity.com>
2020-01-10 21:22:34 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# go_miner
+go_miner is a SQL database miner written in golang. 
+
+**Author**: [th3jiv3r][twitter]
+
+### New Features!
+  - map MS-SQL server databases
+  - uses regex to look for specified keywords in table columns, showing if there are any matches
+
+### Installation
+#### Setup and build go_miner
+```sh
+$ go get github.com/goodlandsecurity/go_miner/dbminer
+$ git clone https://github.com/goodlandsecurity/go_miner
+$ cd go_miner
+$ go build go_miner.go
+```
+#### Setup test MS-SQL docker instance
+Use the transactions_seed.txt file to build the application database if you want to have two databases configured
+```sh
+$ docker run --name mssql -p 1433:1433 -e 'ACCEPT_EULA=Y' \
+-e 'SA_PASSWORD=P@$$w0rd!' -d microsoft/mssql-server-linux
+$ docker exec -it mssql /opt/mssql-tools/bin/sqlcmd -S localhost \
+-U sa -P 'P@$$w0rd!'
+> CREATE DATABASE store;
+> GO
+> USE store;
+> CREATE TABLE transactions(ccnum varchar(32), date date, amount decimal(7,2),cvv char(4), exp date);
+> GO
+> INSERT INTO transactions(ccnum, date, amount, cvv, exp) VALUES('4444333322221111', '2019-01-05', 100.12, '1234', '2020-09-01');
+> INSERT INTO transactions(ccnum, date, amount, cvv, exp) VALUES('4444123456789012', '2019-01-07', 2400.18, '5544', '2021-02-01');
+> INSERT INTO transactions(ccnum, date, amount, cvv, exp) VALUES('4465122334455667', '2019-01-29', 1450.87, '9876', '2020-06-01');
+> GO
+
+### Example Use:  
+  - *go_miner -host localhost -user sa -password P@$$w0rd!*
+  - *go_miner -host 10.0.0.1 -port 1234 -user sa -password P@$$w0rd!*
+  - *go_miner -host 192.168.0.1 -user sa -password P@$$w0rd! -debug true* 
+
+#### License
+  - GNU General Public License v3.0
+
+
+[twitter]: <https://twitter.com/th3_jiv3r>
---
+++ cmd/dbminer/go_miner.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"log"
+
+	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/goodlandsecurity/go_dbminer/dbminer"
+)
+
+var (
+	db       *sql.DB
+	server        = flag.String("server", "", "the database server")
+	port     *int = flag.Int("port", 1433, "the database port")
+	user          = flag.String("user", "", "the database user")
+	password      = flag.String("password", "", "the database password")
+	debug         = flag.Bool("debug", false, "enable debugging")
+)
+
+type MSSQLMiner struct {
+	Host *string
+	Db   sql.DB
+}
+
+func New(host *string) (*MSSQLMiner, error) {
+	m := MSSQLMiner{Host: host}
+	err := m.connect()
+	if err != nil {
+		return nil, err
+	}
+	return &m, nil
+}
+
+func (m *MSSQLMiner) connect() error {
+	if *debug {
+		fmt.Printf(" server:%s\n", *server)
+		fmt.Printf(" port:%d\n", *port)
+		fmt.Printf(" user:%s\n", *user)
+		fmt.Printf(" password:%s\n", *password)
+	}
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%v;port=%d;database=%s;",
+		*server,
+		*user,
+		*password,
+		*port,
+		*database)
+	if *debug {
+		fmt.Printf(" connString:%s\n", connString)
+	}
+
+	db, err := sql.Open("sqlserver", connString)
+	if err != nil {
+		log.Fatal("Failed to connect:", err.Error())
+	}
+	fmt.Println("Connected to server!\n")
+	m.Db = *db
+	return nil
+}
+
+func (m *MSSQLMiner) GetSchema() (*dbminer.Schema, error) {
+	var (
+		name    string
+		dbnames []string
+	)
+	var s = new(dbminer.Schema)
+
+	dbsql := fmt.Sprintf("SELECT name FROM sys.databases WHERE name NOT IN ('master', 'tempdb', 'model', 'msdb')")
+
+	fmt.Println("Searching for databases!\n")
+	dbrows, err := m.Db.Query(dbsql)
+	if err != nil {
+		log.Fatal("Scan failed!")
+	}
+	defer dbrows.Close()
+
+	for dbrows.Next() {
+		err := dbrows.Scan(&name)
+		if err != nil {
+			log.Fatal("Scan failed:", err.Error())
+		}
+		dbnames = append(dbnames, name)
+
+	}
+
+	for _, dbname := range dbnames {
+		tsql := fmt.Sprintf("USE %s; SELECT table_catalog as table_schema, table_name, column_name FROM information_schema.columns ORDER BY table_schema, table_name", dbname)
+
+		schemarows, err := m.Db.Query(tsql)
+		if err != nil {
+			log.Fatal("Query failed:", err.Error())
+		}
+		defer schemarows.Close()
+
+		var prevschema, prevtable string
+		var db dbminer.Database
+		var table dbminer.Table
+		for schemarows.Next() {
+			var table_schema, table_name, column_name string
+			err := schemarows.Scan(&table_schema, &table_name, &column_name)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if table_schema != prevschema {
+				if prevschema != "" {
+					db.Tables = append(db.Tables, table)
+					s.Databases = append(s.Databases, db)
+				}
+				db = dbminer.Database{Name: table_schema, Tables: []dbminer.Table{}}
+				prevschema = table_schema
+				prevtable = ""
+			}
+
+			if table_name != prevtable {
+				if prevtable != "" {
+					db.Tables = append(db.Tables, table)
+				}
+				table = dbminer.Table{Name: table_name, Columns: []string{}}
+				prevtable = table_name
+			}
+			table.Columns = append(table.Columns, column_name)
+		}
+		db.Tables = append(db.Tables, table)
+		s.Databases = append(s.Databases, db)
+		if err := schemarows.Err(); err != nil {
+			return nil, err
+		}
+	}
+	return s, nil
+}
+
+func main() {
+	flag.Parse()
+	mm, err := New(server)
+	if err != nil {
+		panic(err)
+	}
+	defer mm.Db.Close()
+
+	if err := dbminer.Search(mm); err != nil {
+		panic(err)
+	}
+}
